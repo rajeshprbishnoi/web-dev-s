6fdfe7bf21309da923f91b662b89aa0d967fbadd
Rajesh Bishnoi <rajeshprbishnoi@gmail.com>
2022-08-23 17:40:54 +0530

    functions in JS

--- js-basics/data-type.js
+++ js-basics/data-type.js
@@ -2,13 +2,19 @@ function a() {
 	return 1;
 }
 
+// int and float are Number in js
 console.log(1234, typeof 1234); // Output : Number
 console.log(12.34, typeof 12.34); // Output : Number
+
+// string and character are same in js
 console.log("a", typeof "a"); // Output : string
 console.log("absd", typeof "absd"); // Output : string
+
 console.log(null, typeof null); // Output : object
 console.log(undefined, typeof undefined); // Output : undefined
 
+// because it's a function call
 console.log(a(), typeof a()); // output : 1, number
 
+// function is a data type in js
 console.log(a, typeof a); // output of [typeof a] : function
---
+++ js-basics/functions.js
@@ -0,0 +1,56 @@
+//Hoisting
+
+/*
+In hoisting function defination and variable (declared with var) are kept on top and other(unexecuted)statement
+are put into a queue
+*/
+console.log(beta()); // because of hoisting this statement won't be an error
+
+function alpha() {
+	return "a";
+}
+
+function beta() {
+	return "b";
+}
+
+// Uncaught ReferenceError ;
+/*
+//Gamma works as an variable
+
+ console.log(gamma); 
+*/
+
+let gamma = function () {
+	return "g";
+};
+
+// Polymorphism
+
+function area(height, width) {
+	return height * width;
+}
+
+console.log("area 3,4 : ", area(3, 4));
+
+// because width is undefined here output must be NaN
+console.log("Area 4 : ", area(4));
+/*
+ In order to achieve polymorphism in JS we use if condition,
+ If we try to define the function again to achieve polymorphisn (C++ & Java)
+ we'll be overwriting the previous defined function
+*/
+
+// arguments array object :
+/*
+It's an array object available in every function by default, if we are not accepting any arguments in function
+and at the time of function call arguments are passed then we can access those arguments using arguments array
+object 
+*/
+
+function hello() {
+	console.log("Hello World! " + arguments[0] + " " + arguments[1]);
+}
+
+hello(1, 2);
+hello("Rajesh", "Bishnoi");
