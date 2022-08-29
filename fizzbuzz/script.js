ee0a57cf5832c06fa1b9ed051daa8e320012d807
Rajesh Bishnoi <rajeshprbishnoi@gmail.com>
2022-08-29 17:13:25 +0530

    FizzBuzz optimised

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -8,14 +8,32 @@ btnPrint.onclick = function () {
 	let num = Number(inpNum.value);
 
 	let count = 1;
+	let c3 = 0,
+		c5 = 0;
 	while (count <= num) {
 		let node = document.createElement("li");
 		let text = "";
+
+		c3++, c5++;
+
+		if (c3 == 3) {
+			text += "Fizz";
+			c3 = 0;
+		}
+		if (c5 == 5) {
+			text += "Buzz";
+			c5 = 0;
+		}
+		if (text === "") text = count;
+		node.textContent = text;
+
+		/*
 		if (count % 3 == 0) text += "Fizz";
 		if (count % 5 == 0) text += "Buzz";
 		if (text === "") text = count;
 
 		node.textContent = text;
+        */
 
 		/*
         if (count % 15 === 0) {
